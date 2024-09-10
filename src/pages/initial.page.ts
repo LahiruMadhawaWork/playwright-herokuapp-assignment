1fa7f33a483f586a19f770c888e43795408202ac
LahiruMadhawaWork <lahirumadhawawork@gmail.com>
2024-09-10 09:59:01 +0530

    added .\src\pages\initial.page.ts file

---
+++ src/pages/initial.page.ts
@@ -0,0 +1,19 @@
+import { Page } from "@playwright/test";
+import Wrapper from "../base/Wrapper";
+
+export default class InitialPage extends Wrapper {
+    // Constructor
+    constructor(public page: Page) {
+        super(page);
+    }
+
+    // methods
+    public async navigateToPage(page: Page, url: string) {
+        await page.goto(url);
+    }
+
+    public async clickLinkElements(elementname: string){
+        const linkElement = await this.findLocator("//div[@id='content']//a[@href and(text()='" + elementname + "')]")
+        await linkElement?.click();
+    }
+}
