57f3bc714955b8308ec8bf78eb3e3d9924617a60
LahiruMadhawaWork <lahirumadhawawork@gmail.com>
2024-09-15 22:36:29 +0530

    updated the test heading

--- tests/contextmenuPage.test.ts
+++ tests/contextmenuPage.test.ts
@@ -25,7 +25,7 @@ test("ContextMenu link selection @Smoke", async (testInfo) => {
         await objInitialPage.assertInitialPageURL();
     })
     
-    await test.step("Click on ChallengingDOM link", async () => {
+    await test.step("Click on ContextMenu link", async () => {
         await objInitialPage.clickOnContextMenuLink();
         await objContextMenuPage.assertContextMenuURL();
         await objContextMenuPage.assertForHeaderContextMenu();
